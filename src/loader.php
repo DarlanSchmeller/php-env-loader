87a44f5eb97c81501b798456062734b08fafea29
Darlan Rodrigues Schmeller <darlanschimeller@gmail.com>
2026-01-14 10:49:21 -0300

    refactor: Implement regex splitting to handle inline comments

--- src/loader.php
+++ src/loader.php
@@ -7,29 +7,27 @@ if (! file_exists('../.env')) {
 
 // Read the .env file
 $lines = file('../.env',  FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-$envValues = [];
 
 // Loop line by line
 foreach($lines as $line) {
     $line = trim($line);
-
     // Skip empty lines or comments
-    if ($line === '' || str_starts_with('#', $line)) {
+    if ($line === '' || str_starts_with($line, '#')) {
         continue;
     }
 
     // Check for inline commnets
-    if (str_contains($line, '#')) {
+    if (preg_match('/\s+#/', $line)) {
         // Get key and value before comment
-        $line = trim(explode('#', $line)[0]);
+        $line = trim(preg_split('/\s+#/', $line, 2)[0]);
     };
 
     // Parse key and value
     [$key, $value] = explode('=', $line, 2);
-    trim($key);
-    trim($value);
+    $key = trim($key);
+    $value = trim($value);
 
-    if ($key === '' || $value === '') {
+    if ($key === '') {
         continue;
     }
 
